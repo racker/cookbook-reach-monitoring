754223e02910121f4b7889d04ecdd5244a8e9560
Matthew Green <thematthewgreen@gmail.com>
2014-05-27 15:09:47 -0400

    Update metadata.rb
    Newer Berkshelf version requires a name attribute

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@
 maintainer       'Rackspace'
 maintainer_email 'cooks@lists.rackspace.com'
 license          'All rights reserved'
+name             'cookbook-reach-monitoring'
 description      'Installs/Configures Cloud Monitoring Base Checks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
